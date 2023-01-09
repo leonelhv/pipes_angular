a013a8a2aa9c37568f45568aabb12613a4d4fbf4
Leonel Huachaca <leonel.huachacav@gmail.com>
2023-01-09 09:41:30 -0500

    import formsModule

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UppercaseComponent } from './uppercase/uppercase.component';
 import { CurrencyComponent } from './currency/currency.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [UppercaseComponent, CurrencyComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   exports: [UppercaseComponent, CurrencyComponent],
 })
 export class PagesModule {}
