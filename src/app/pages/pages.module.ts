6c3cbea1bfe70bed155932a8b782ac9d98bb574d
Leonel Huachaca <leonel.huachacav@gmail.com>
2023-01-09 10:01:11 -0500

    import router Module

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { UppercaseComponent } from './uppercase/uppercase.component';
 import { CurrencyComponent } from './currency/currency.component';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [UppercaseComponent, CurrencyComponent],
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterModule],
   exports: [UppercaseComponent, CurrencyComponent],
 })
 export class PagesModule {}
