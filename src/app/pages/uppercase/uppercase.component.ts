e8afe86114015abeaa456d1b7846a5c0f416aff8
Leonel Huachaca <leonel.huachacav@gmail.com>
2023-01-09 09:47:17 -0500

    add uppercase component and finish

---
+++ src/app/pages/uppercase/uppercase.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-uppercase',
+  templateUrl: './uppercase.component.html',
+  styleUrls: ['./uppercase.component.css'],
+})
+export class UppercaseComponent {
+  resultado: string = '';
+}
